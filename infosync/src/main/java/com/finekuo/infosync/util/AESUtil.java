af5fa062d9b9073dc9fd7e44b5117c49334484f9
eric.kuo <fine1101105351@gmail.com>
2025-03-27 02:45:26 +0800

    init

--- infosync/src/main/java/com/finekuo/infosync/util/AESUtil.java
+++ infosync/src/main/java/com/finekuo/infosync/util/AESUtil.java
@@ -3,6 +3,7 @@ package com.finekuo.infosync.util;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 public class AESUtil {
@@ -12,7 +13,7 @@ public class AESUtil {
     // 產生 AES 金鑰
     public static SecretKey generateAESKey() throws Exception {
         KeyGenerator keyGen = KeyGenerator.getInstance(AES_ALGORITHM);
-        keyGen.init(128); // 可選擇 128、192、256
+        keyGen.init(256); // 可選擇 128、192、256
         return keyGen.generateKey();
     }
 
@@ -20,7 +21,7 @@ public class AESUtil {
     public static String encryptAES(String plainText, SecretKey secretKey) throws Exception {
         Cipher cipher = Cipher.getInstance(AES_ALGORITHM);
         cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-        byte[] encryptedBytes = cipher.doFinal(plainText.getBytes("UTF-8"));
+        byte[] encryptedBytes = cipher.doFinal(plainText.getBytes(StandardCharsets.UTF_8));
         return Base64.getEncoder().encodeToString(encryptedBytes);
     }
 
@@ -29,7 +30,7 @@ public class AESUtil {
         Cipher cipher = Cipher.getInstance(AES_ALGORITHM);
         cipher.init(Cipher.DECRYPT_MODE, secretKey);
         byte[] decodedBytes = Base64.getDecoder().decode(encryptedText);
-        return new String(cipher.doFinal(decodedBytes), "UTF-8");
+        return new String(cipher.doFinal(decodedBytes), StandardCharsets.UTF_8);
     }
 
 }
