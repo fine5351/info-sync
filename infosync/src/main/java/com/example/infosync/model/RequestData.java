845b4994e1664f0271231b3c1ad8d8424629287c
eric.kuo <fine1101105351@gmail.com>
2025-04-14 22:48:52 +0800

    init

---
+++ infosync/src/main/java/com/example/infosync/model/RequestData.java
@@ -0,0 +1,11 @@
+package com.example.infosync.model;
+
+import lombok.Data;
+
+@Data
+public class RequestData {
+    private Long id;
+    private String name;
+    private String rocId;
+    private String address;
+} 
